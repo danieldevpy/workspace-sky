e255853c590762b830c78257872cbf8a8fb6f615
Pc Daniel <danielfernandes202@gmail.com>
2025-04-02 15:18:13 -0300

    fix ambiente desenvolvimento

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -113,12 +113,8 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("MYSQL_DATABASE"),
-        'USER': os.getenv("MYSQL_USER"),
-        'PASSWORD': os.getenv("MYSQL_PASSWORD"),
-        'HOST': os.getenv("MYSQL_HOST"),
-        'PORT': os.getenv("MYSQL_PORT"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
